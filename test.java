7a024c1be4e5c2bf07d55a5dfa17a972a02e8968
DeclanDavis <declan.davis.2024@mumail.ie>
2024-07-25 23:14:58 +0100

    added a gitNotes.md file

--- test.java
+++ test.java
@@ -5,10 +5,15 @@ class test {
     public static void main(String[] args) {
 
         int num = 3;
+        out.println(isEven(num));
         
         /* Comment added */
         out.println("hello world");
         out.println("new line");
     }
 
+    public static boolean isEven(int x){
+        if(x % 2 == 0) return true;
+        return false;
+    }
 }
